2134755699197d1f7d976e0aa6471d06d29e5e72
Cy Ellison <dragonz2626@gmail.com>
2018-09-20 13:00:15 -0600

    added some basic code for user to enter varibales

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,10 +1,55 @@
 package controller;
 import model.Football;
+import javax.swing.JOptionPane;
 
 public class Controller
 {
+	private Football userTeam;
+	public Controller()
+	{
+		userTeam = new Football();
+	}
 	public void start()
 	{
+		JOptionPane.showMessageDialog(null, "You will enter in all the info for your team during a game!");
+		JOptionPane.showMessageDialog(null, "All your info will be shown at the end!");
+		JOptionPane.showMessageDialog(null, "Please enter in the right variables! If you don't it wont work!");
 		
+		String userInput = JOptionPane.showInputDialog("What is your teams name?");
+		userTeam.setTeamName(userInput);
+		
+		userInput = JOptionPane.showInputDialog("What quarter is it?");
+		int quarter = 0;
+		if(validInt(userInput))
+		{
+			quarter = Integer.parseInt(userInput);
+		}
+		if(quarter >= 5)
+		{
+			quarter = 4;
+		}
+		else if(quarter <= 0)
+		{
+			quarter = 1;
+		}
+
+		userTeam.setQuarter(quarter);
+		
+	}
+	public boolean validInt(String maybeInt)
+	{
+		
+		boolean isValid = false;
+		
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch (NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "You need to type in a whole number :D");
+		}
+		return isValid;
 	}
 }
