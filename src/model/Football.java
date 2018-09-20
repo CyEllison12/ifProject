5530626fe6d43bd4d64f2c7b6e48e7e98275a716
Cy Ellison <dragonz2626@gmail.com>
2018-09-20 12:22:03 -0600

    added javadoc for the constructors

--- src/model/Football.java
+++ src/model/Football.java
@@ -16,11 +16,29 @@ public class Football
 	private boolean hasBall;
 	//--------------------------------------------------------
 	
-	
+	/**
+	 * Default constructor for a Football game.
+	 * Initializes all value to valid but "bad" values
+	 * Used for later customization
+	 */
 	public Football()
 	{
-		//default constructor
+		this.timeMinutes = -99;
+		this.timeSeconds = -99;
+		this.scoreHome = -99;
+		this.scoreAway = -99;
+		this.teamName = "No team";
+		this.against = "Not playing against anyone!";
+		this.quarter = -99;
+		this.ballPos = -99;
+		this.down = -99;
+		this.yardsFirstDown = -99;
+		this.hasBall = false;
 	}
+	/**
+	 * Initializes a Football team name.
+	 * @param name The team name.
+	 */
 	public Football(String name)
 	{
 		this.teamName = name;
