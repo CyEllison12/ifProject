e4d200d283bb45d16af28c43e40b86c46913996a
Cy Ellison <dragonz2626@gmail.com>
2018-09-20 12:04:18 -0600

    added basic constructors, added variables needed

--- src/model/Football.java
+++ src/model/Football.java
@@ -2,5 +2,29 @@ package model;
 
 public class Football
 {
+	//-------------Declaration Section------------------------
+	private int timeMinutes;
+	private int timeSeconds;
+	private int score;
+	private String teamName;
+	private String against;
+	private int quarter;
+	private int ballPos;
+	private int down;
+	private int yardsFirstDown;
+	private boolean hasBall;
+	//--------------------------------------------------------
+	
+	
+	public Football()
+	{
+		
+	}
+	public Football(String name)
+	{
+		this.teamName = name;
+	}
+	
+	
 
 }
